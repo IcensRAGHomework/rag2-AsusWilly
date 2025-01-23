8b6333a6caf5c797f4566d3369abfe53b50735f1
Willy_Lin <willy_lin@asus.com>
2025-01-23 16:34:43 +0800

    [RAG] Homework 2-1

--- student_assignment.py
+++ student_assignment.py
@@ -1,5 +1,5 @@
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import (CharacterTextSplitter,
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import (CharacterTextSplitter,
                                       RecursiveCharacterTextSplitter)
 
 q1_pdf = "OpenSourceLicenses.pdf"
@@ -7,7 +7,24 @@ q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    loader = PyPDFLoader(q1_pdf)
+    documents = loader.load()
+    splitter = CharacterTextSplitter(chunk_overlap = 0)
+    for i, doc in enumerate(documents):
+        page_number = i + 1
+        chunks = splitter.split_text(doc.page_content)
+        for j, chunk in enumerate(chunks):
+            last_chunk_info = {
+                'filename': q1_pdf,
+                'page': page_number,
+                'content': chunk
+            }
+    print(f"Filename: {last_chunk_info['filename']}")
+    print(f"Page: {last_chunk_info['page']}")
+    print(f"Content:\n{last_chunk_info['content']}")
 
 def hw02_2(q2_pdf):
     pass
+
+hw02_1(q1_pdf)
+# hw02_1(q2_pdf)
