fab5a7d4ad67976530e08c8971ec4de3f89b388d
Willy_Lin <willy_lin@asus.com>
2025-01-23 17:28:52 +0800

    [RAG] Homework 2-1

--- student_assignment.py
+++ student_assignment.py
@@ -1,3 +1,4 @@
+import re
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import (CharacterTextSplitter,
                                       RecursiveCharacterTextSplitter)
@@ -16,8 +17,21 @@ def hw02_1(q1_pdf):
 def hw02_2(q2_pdf):
     loader = PyPDFLoader(q2_pdf)
     documents = loader.load()
-    splitter = RecursiveCharacterTextSplitter(chunk_overlap = 0)
-    chunks = splitter.split_documents(documents)
+    full_text = " ".join([doc.page_content for doc in documents])
+    pattern = r'(第[一二三四五六七八九十百千]+[章條])'
+    sections = re.split(pattern, full_text)
+    chunks = []
+    for i in range(1, len(sections), 2): 
+        title = sections[i]
+        content = sections[i + 1] if i + 1 < len(sections) else ''
+        full_chunk = title + content
+        chunks.append(full_chunk)
+
+    final_chunks = []
+    text_splitter = RecursiveCharacterTextSplitter(chunk_overlap=0)
+    for chunk in chunks:
+        split_chunks = text_splitter.split_text(chunk)
+        final_chunks.extend(split_chunks)
     return len(chunks)
 
 # print(hw02_1(q1_pdf))
