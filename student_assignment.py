19126ac706b6346e87cee57b86982c975377a62e
Willy_Lin <willy_lin@asus.com>
2025-01-23 17:00:29 +0800

    [RAG] Homework 2-1

--- student_assignment.py
+++ student_assignment.py
@@ -11,18 +11,13 @@ def hw02_1(q1_pdf):
     documents = loader.load()
     splitter = CharacterTextSplitter(chunk_overlap = 0)
     for i, doc in enumerate(documents):
-        page_number = i + 1
         chunks = splitter.split_text(doc.page_content)
         for j, chunk in enumerate(chunks):
-            last_chunk_info = {
-                'filename': q1_pdf,
-                'page': page_number,
-                'content': chunk
-            }
+            last_chunk_info = chunk
     return last_chunk_info
 
 def hw02_2(q2_pdf):
     pass
 
-hw02_1(q1_pdf)
+print(hw02_1(q1_pdf))
 # hw02_1(q2_pdf)
