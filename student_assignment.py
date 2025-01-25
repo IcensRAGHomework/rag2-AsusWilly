c23f2c5a8b67d892ac2d91692a65bd619ff04d49
Willy_Lin <willy_lin@asus.com>
2025-01-25 09:00:16 +0800

    [RAG] Homework 2-1

--- student_assignment.py
+++ student_assignment.py
@@ -17,21 +17,14 @@ def hw02_1(q1_pdf):
 def hw02_2(q2_pdf):
     loader = PyPDFLoader(q2_pdf)
     documents = loader.load()
-    full_text = " ".join([doc.page_content for doc in documents])
-    pattern = r'(第[一二三四五六七八九十百千]+[章條])'
-    sections = re.split(pattern, full_text)
-    chunks = []
-    for i in range(1, len(sections), 2): 
-        title = sections[i]
-        content = sections[i + 1] if i + 1 < len(sections) else ''
-        full_chunk = title + content
-        chunks.append(full_chunk)
-
-    final_chunks = []
-    text_splitter = RecursiveCharacterTextSplitter(chunk_overlap=0)
-    for chunk in chunks:
-        split_chunks = text_splitter.split_text(chunk)
-        final_chunks.extend(split_chunks)
+    full_text = '\f'.join([doc.page_content for doc in documents])
+    pattern = [r'第\s[零一二三四五六七八九十]+\s章', r'第\s[\d-]+\s條']
+    chunks = RecursiveCharacterTextSplitter(
+        separators = pattern,
+        chunk_overlap = 0,
+        chunk_size = 1,
+        is_separator_regex = True,
+        keep_separator = True).split_text(full_text)
     return len(chunks)
 
 # print(hw02_1(q1_pdf))
